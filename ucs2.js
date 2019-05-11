86d8711a5748a85cbeff2fa6d1c2ed856dd8c434
asilinwei <1021069119@qq.com>
2019-05-11 10:52:33 +0800

    init

---
+++ ucs2.js
@@ -0,0 +1,53 @@
+function ucs2Encode(array) {
+  "use strict";
+
+  if (!Array.isArray(array)) {
+    throw new Error('Not array');
+  }
+
+  var result = '',
+      length = array.length;
+
+  for (var i = 0; i < length; i += 1) {
+    var codePoint = array[i];
+    if (!Number.isInteger(codePoint) || codePoint < 0 || codePoint > 0x10FFFF) {
+      throw new Error('Invalid code point');
+    }
+    if (codePoint > 0xFFFF) {
+      var diff = codePoint - 0x10000;
+      result += String.fromCharCode(0xD800 | (diff >> 10)) + String.fromCharCode(0xDC00 | (diff & 0x3FF));
+    } else {
+      result += String.fromCharCode(codePoint);
+    }
+  }    
+  return result;
+}
+
+function ucs2Decode(string) {
+  "use strict";
+
+  if (typeof string !== 'string') {
+    throw new Error('Not string');
+  }
+
+  var result = [],
+      index = -1,
+      length = string.length;  
+       
+  while (++index < length) {
+    var value = string.charCodeAt(index);
+    if (value >= 0xD800 && value <= 0xDBFF && index < length) {
+      var next = string.charCodeAt(++index);
+      if (next >= 0xDC00 && next <= 0xDFFF) {
+        result.push((((value & 0x3FF) << 10) | (next & 0x3FF)) + 0x10000);
+      } else {
+        index -= 1;
+        result.push(value);
+      }
+    } else {
+      result.push(value);
+    }
+  } 
+  return result;    
+}
+
